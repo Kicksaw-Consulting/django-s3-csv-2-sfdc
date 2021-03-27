f2a13e30ebab92acee2f9a17ac00fb83ba6ae070
Alex Drozd <drozdster@gmail.com>
2021-03-27 17:37:05 +0100

    adding get_timestamp_folder

--- django_s3_csv_2_sfdc/utils.py
+++ django_s3_csv_2_sfdc/utils.py
@@ -16,3 +16,22 @@ def get_iso() -> str:
     Get an iso timestamp with less precision than .iso() returns, and no special characters
     """
     return datetime.datetime.now().strftime("%Y%m%dT%H%M")
+
+
+def get_timestamp_folder(datetime_object: datetime.datetime = None) -> Path:
+    """
+    Returns a Path object that's a timestamped folder path
+
+    e.g., ts_folder = 2021/03/27
+        so you can do Path("some/path") / ts_folder / <your_file>.txt
+
+    Defaults to datetime.datetime.now() if nothing's passed in
+    """
+    if not datetime_object:
+        datetime_object = datetime.datetime.now()
+
+    year = datetime_object.strftime("%Y")
+    month = datetime_object.strftime("%m")
+    day = datetime_object.strftime("%d")
+
+    return Path(year) / month / day
---
+++ tests/test_utils.py
@@ -0,0 +1,9 @@
+import datetime
+
+from django_s3_csv_2_sfdc.utils import get_timestamp_folder
+
+
+def test_get_timestamp_folder():
+    dt = datetime.datetime(year=2021, month=3, day=27)
+    folder = get_timestamp_folder()
+    assert "2021/03/27" == folder.as_posix()
