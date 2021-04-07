e40169bc9c530670c9cb7209d9d7b02b3c545e4a
Alex Drozd <drozdster@gmail.com>
2021-04-07 16:18:04 +0200

    fixing test and adding github actions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,5 +5,5 @@ from django_s3_csv_2_sfdc.utils import get_timestamp_folder
 
 def test_get_timestamp_folder():
     dt = datetime.datetime(year=2021, month=3, day=27)
-    folder = get_timestamp_folder()
+    folder = get_timestamp_folder(dt)
     assert "2021/03/27" == folder.as_posix()
