03b2eb6451a39f8926e4d5a3fe191aecee4f46b8
Alex Drozd <drozdster@gmail.com>
2021-06-25 12:28:06 -0500

    beefing up the test

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,4 +1,6 @@
 import csv
+import os
+
 from pathlib import Path
 
 from tempfile import gettempdir
@@ -60,29 +62,45 @@ def test_orchestrator(monkeypatch):
 
     # imagine we pushed to salesforce
     orchestrator.log_batch(results, data, "Contact", "ID")
+
+    with open(orchestrator.error_report_path) as error_report:
+        csv_reader = csv.DictReader(error_report)
+
+        count = 0
+        for idx, row in enumerate(csv_reader):
+            assert row["code"] == "DIDNT_WORK"
+            assert row["message"] == "it broke"
+            assert row["upsert_key_value"] == "1"
+            assert row["upsert_key"] == "ID"
+            assert row["salesforce_object"] == "Contact"
+            count += 1
+        assert count == 1
+
+    # test appending
+    data_2 = [{"ID": 4, "Name": "A"}, {"ID": 5, "Name": "B"}, {"ID": 6, "Name": "C"}]
     results_2 = [
         {
-            "success": True,
+            "success": False,
             "created": True,
-            "Id": 1,
+            "Id": 4,
             "errors": [{"statusCode": "WEIRD_FAIL_1", "message": "it is broken 1"}],
         },
         {
-            "success": True,
+            "success": False,
             "created": True,
-            "Id": 2,
+            "Id": 5,
             "errors": [{"statusCode": "WEIRD_FAIL_2", "message": "it is broken 2"}],
         },
         {
-            "success": True,
+            "success": False,
             "created": True,
-            "Id": 3,
+            "Id": 6,
             "errors": [{"statusCode": "WEIRD_FAIL_3", "message": "it is broken 3"}],
         },
     ]
     orchestrator.log_batch(
         results_2,
-        data,
+        data_2,
         "Contact",
         "ID",
     )
@@ -91,7 +109,7 @@ def test_orchestrator(monkeypatch):
 
     with open(orchestrator.error_report_path) as error_report:
         csv_reader = csv.DictReader(error_report)
-
+        count = 0
         for idx, row in enumerate(csv_reader):
             if idx == 0:
                 assert row["code"] == "DIDNT_WORK"
@@ -102,9 +120,25 @@ def test_orchestrator(monkeypatch):
             elif idx == 1:
                 assert row["code"] == "WEIRD_FAIL_1"
                 assert row["message"] == "it is broken 1"
-                assert row["upsert_key_value"] == "1"
+                assert row["upsert_key_value"] == "4"
                 assert row["upsert_key"] == "ID"
                 assert row["salesforce_object"] == "Contact"
+            elif idx == 2:
+                assert row["code"] == "WEIRD_FAIL_2"
+                assert row["message"] == "it is broken 2"
+                assert row["upsert_key_value"] == "5"
+                assert row["upsert_key"] == "ID"
+                assert row["salesforce_object"] == "Contact"
+            elif idx == 3:
+                assert row["code"] == "WEIRD_FAIL_3"
+                assert row["message"] == "it is broken 3"
+                assert row["upsert_key_value"] == "6"
+                assert row["upsert_key"] == "ID"
+                assert row["salesforce_object"] == "Contact"
+            count += 1
+        assert count == 4
 
     assert orchestrator.archive_file_s3_key == f"archive/junk-{timestamp}.csv"
     assert orchestrator.error_file_s3_key == f"errors/error-report-{timestamp}.csv"
+
+    os.remove(orchestrator.error_report_path)
